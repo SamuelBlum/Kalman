55bf29b06b70424a24be03f7233ea4c70e39499f
Samuel <Sam@FalconMDPA.com>
2019-07-23 16:32:09 -0400

    update

--- kalman.py
+++ kalman.py
@@ -1,53 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.rcParams['figure.figsize'] = (10, 8)
+plt.rcParams['figure.figsize'] = (8, 4)
 
 # intial parameters
-n_iter = 50
-sz = (n_iter,) # array size
 x = -0.14 # truth
-z = np.random.normal(x,0.1,size=sz) # observations (normal about x, sigma=0.1)
-
-Q = 1e-5 # process variance
-
-# allocate space for arrays
-xhat=np.zeros(sz)      # estimate of x
-P=np.zeros(sz)         # error estimate
-xhatminus=np.zeros(sz) # estimate of x
-Pminus=np.zeros(sz)    # error estimate
-K=np.zeros(sz)         # gain
+n_iter = 50
+size = (n_iter,) # size of the array
+z = np.random.normal(x,0.2,size=size) # normal about x, sigma=0.2
 
 R = 0.1**2 # estimate of measurement variance
+Q = 1e-5 # process variance
 
-# intial guesses
-xhat[0] = 0.0
+P=np.zeros(size)         # error estimate
+Pm=np.zeros(size)    # error estimate
+xhm=np.zeros(size) # estimate of x
+xh=np.zeros(size)      # estimate of x
+K=np.zeros(size)         # gain
+
+#init guess of truth
+xh[0] = 1.0
 P[0] = 1.0
 
 for k in range(1,n_iter):
     # time update
-    xhatminus[k] = xhat[k-1]
-    Pminus[k] = P[k-1]+Q
+    xhm[k] = xh[k-1]
+    Pm[k] = P[k-1]+Q
 
     # measurement update
-    K[k] = Pminus[k]/( Pminus[k]+R )
-    xhat[k] = xhatminus[k]+K[k]*(z[k]-xhatminus[k])
-    P[k] = (1-K[k])*Pminus[k]
+    K[k] = Pm[k]/( Pm[k]+R )
+    xh[k] = xhm[k]+K[k]*(z[k]-xhm[k])
+    P[k] = (1-K[k])*Pm[k]
 
 plt.figure()
-plt.plot(z,'k+',label='noisy measurements')
-plt.plot(xhat,'b-',label='estimate')
-plt.axhline(x,color='g',label='truth value')
+plt.plot(z,'k+',label='noise plus measurements')
+plt.plot(xh,'b-',label='estimate of truth')
+plt.axhline(x,color='g',label='truth')
 plt.legend()
-plt.title('Estimate vs. iteration step', fontweight='bold')
-plt.xlabel('Iteration')
+plt.title('Estimate vs. time', fontweight='bold')
+plt.xlabel('Time')
 plt.ylabel('Amplitude')
-
-plt.figure()
-valid_iter = range(1,n_iter) # Pminus not valid at step 0
-plt.plot(valid_iter,Pminus[valid_iter],label='Error estimate')
-plt.title('Estimated error vs. iteration step', fontweight='bold')
-plt.xlabel('Iteration')
-plt.ylabel('$(Amplitude)^2$')
-plt.setp(plt.gca(),'ylim',[0,.01])
 plt.show()
